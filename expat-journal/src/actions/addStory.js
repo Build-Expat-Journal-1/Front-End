6cc1de54158bf04d71bcdc208eee0a323d53233a
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-06-24 07:14:36 -0400

    add story feature

--- expat-journal/src/actions/addStory.js
+++ expat-journal/src/actions/addStory.js
@@ -4,10 +4,11 @@ export const ADD_STORY_START = 'ADD_STORY_START'
 export const ADD_STORY_SUCCESS = 'ADD_STORY_SUCCESS'
 export const ADD_STORY_FAIL = 'ADD_STORY_FAIL'
 
-export const addStory = (story) => dispatch => {
+export const addStory = (story, id) => dispatch => {
     dispatch({ type: ADD_STORY_START, payload: story})
-    axios.post('https://bw-expatjournal.herokuapp.com/api/stories/:id/my-stories', story)
+    axios.post(`https://bw-expatjournal.herokuapp.com/api/stories/${id}/my-stories`, story)
     .then(res => {
+        console.log('i added the story!')
         dispatch({type: ADD_STORY_SUCCESS, payload: res.data})
     })
     .catch(err => {
